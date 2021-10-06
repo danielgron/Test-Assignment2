adf3e1b98d696c97d6685935a2779d2a5795bb7d
Daniel Grønbjerg <danielgronbjerg@gmail.com>
2021-10-06 11:23:46 +0200

    Add Bowlinggame Kata as described in ppt

---
+++ BowlingGameKata/src/main/java/dk/danielgron/bowlinggamekata/Roll.java
@@ -0,0 +1,4 @@
+package dk.danielgron.bowlinggamekata;
+
+public class Roll {
+}
--- BowlingGameKata/src/test/java/dk/danielgron/bowlinggamekata/BowlingGameKataApplicationTests.java
+++ BowlingGameKata/src/test/java/dk/danielgron/bowlinggamekata/BowlingGameKataApplicationTests.java
@@ -1,13 +1,69 @@
 package dk.danielgron.bowlinggamekata;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
-class BowlingGameKataApplicationTests {
+class BowlingGameKataApplicationTests{
+    private Game g;
+
+    @BeforeEach
+    protected void setup(){
+        g = new Game();
+    }
+
+    private void rollMany(int n, int pins) {
+        for (int i = 0; i < n; i++)
+            g.roll(pins);
+    }
 
     @Test
-    void contextLoads() {
+    public void testGutterGame(){
+        rollMany(20, 0);
+        assertEquals(0, g.score());
+    }
+
+    @Test
+    public void testAllOnes(){
+        rollMany(20,1);
+        assertEquals(20, g.score());
+
+    }
+
+    @Test
+    public void testOneSpare() {
+        rollSpare();
+        g.roll(3);
+        rollMany(17,0);
+        assertEquals(16,g.score());
+
+    }
+
+    @Test
+    public void testOneStrike(){
+        rollStrike();
+        g.roll(3);
+        g.roll(4);
+        rollMany(16, 0);
+        assertEquals(24, g.score());
+    }
+
+    @Test
+    public void testPerfectGame(){
+        rollMany(12,10);
+        assertEquals(300, g.score());
+    }
+
+    private void rollStrike() {
+        g.roll(10);
+    }
+
+    private void rollSpare() {
+        g.roll(5);
+        g.roll(5);
     }
 
 }
